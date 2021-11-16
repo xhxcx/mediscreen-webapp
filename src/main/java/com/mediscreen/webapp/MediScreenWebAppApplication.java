23b664b9fa32499b918b9485dc35a663a7c6beca
xhxcx <xxhxcxx.sn@gmail.com>
2021-11-16 10:24:15 +0100

    initial commit

---
+++ src/main/java/com/mediscreen/webapp/MediScreenWebAppApplication.java
@@ -0,0 +1,11 @@
+package com.mediscreen.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediScreenWebAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MediScreenWebAppApplication.class, args);
+    }
+}
---
+++ src/test/java/com/mediscreen/webapp/MediScreenWebAppApplicationTests.java
@@ -0,0 +1,11 @@
+package com.mediscreen.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MediScreenWebAppApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
