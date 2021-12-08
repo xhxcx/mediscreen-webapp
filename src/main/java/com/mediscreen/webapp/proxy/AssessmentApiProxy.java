a867255556b95ce84b657cf2457c3bb8c39700d3
xhxcx <xxhxcxx.sn@gmail.com>
2021-12-08 16:28:51 +0100

    fix url mistake in proxy interface + handle empty response of the assessmentapi

--- src/main/java/com/mediscreen/webapp/proxy/AssessmentApiProxy.java
+++ src/main/java/com/mediscreen/webapp/proxy/AssessmentApiProxy.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "assessmentApi", url = "${patientApi.url}:8083")
+@FeignClient(name = "assessmentApi", url = "${assessmentApi.url}:8083")
 public interface AssessmentApiProxy {
 
     @GetMapping("/")
--- src/main/java/com/mediscreen/webapp/service/AssessmentServiceImpl.java
+++ src/main/java/com/mediscreen/webapp/service/AssessmentServiceImpl.java
@@ -3,6 +3,7 @@ package com.mediscreen.webapp.service;
 import com.mediscreen.webapp.model.MediScreenRiskLevel;
 import com.mediscreen.webapp.proxy.AssessmentApiProxy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +14,11 @@ public class AssessmentServiceImpl implements AssessmentService{
 
     @Override
     public MediScreenRiskLevel getRiskLevel(int patientId) {
+        ResponseEntity<MediScreenRiskLevel> response = assessmentApiProxy.getRiskForPatient(patientId);
+        
+        if (!response.hasBody()) {
+            return MediScreenRiskLevel.NOT_APPLICABLE;
+        }
         return assessmentApiProxy.getRiskForPatient(patientId).getBody();
     }
 }
